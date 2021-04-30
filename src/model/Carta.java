042e9d97d2df0cd760a2ea812e0ac64817e597a8
Matheus Teles <matheustdo1@gmail.com>
2021-04-29 22:30:01 -0300

    Cria métodos da view

--- src/model/Carta.java
+++ src/model/Carta.java
@@ -41,15 +41,23 @@ public class Carta {
 	
 	@Override
 	public String toString() {
-		if (this.lado == Lado.BAIXO) return "[< >]";
+		if (this.lado == Lado.BAIXO) return "|< >|";
+		
+		String bordaInicio = "┋";
+		String bordaFim = "┋";
+		
+		if (naipe.getCor() == "Preto") {
+			bordaInicio = "┃";
+			bordaFim = "┃";
+		}
 		
 		/* Para formatar a string da carta, se o valor dela tiver apenas 1 caractere, um espaço
 		 * é inserido para que o conteúdo da carta possua 3 caracteres, exemplo: [5 ♥] */
 		if (numeracao.toString().length() == 1) {
-			return ("[" + numeracao.toString() + " " + naipe.toString() + "]");
+			return (bordaInicio + numeracao.toString() + " " + naipe.toString() + bordaFim);
 		}
 
-		/* Se a carta possuir dois caracteres, ela ficará como: [10♥] */
-		return ("[" + numeracao.toString() + naipe.toString() + "]");
+		/* Se a carta possuir dois caracteres, ela ficará como: [10░♥] */
+		return (bordaInicio + numeracao.toString() + naipe.toString() + bordaFim);
 	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,86 @@
 package view;
 
+import java.util.Scanner;
+
 import controller.Partida;
 
+
 public class Main {
+	private static boolean jogoIniciado = false;
+	
+	private static void limparTela() {
+		for(int i = 0; i < 30; i++) {
+			System.out.println();
+		}
+	}
+	
+	private static void opcoes() {
+		if(jogoIniciado) {
+			System.out.println("1 - Mover carta");
+			System.out.println("2 - Exibir jogo");
+			System.out.println("3 - Alterar nº de cartas a virar do estoque");
+			System.out.println("4 - Reiniciar");
+			System.out.println("5 - Finalizar");
+		} else {
+			System.out.println("4 - Iniciar jogo");
+			System.out.println("5 - Finalizar");
+		}
+	}
+	
+	private static void menu() {
+		String opcao;
+		Scanner entrada = new Scanner(System.in);
+		
+		do{	
+			if(jogoIniciado) limparTela();
+			
+			opcoes();
+			
+			System.out.print("Escolha uma opção: ");
+            opcao = entrada.next();
+            
+            switch(opcao) {
+	            case "1":
+	                break;
+	            case "2":
+	                break;
+	            case "3":
+	                break;
+	            case "4":
+	            	if(jogoIniciado) {
+	            		System.out.print("Você realmente quer reiniciar o jogo? (1 - sim, 2 - não): ");
+	                    opcao = entrada.next();
+	                    
+	                    if(opcao.equals("1")) jogoIniciado = true;;
+	            	} else {
+	                	new Partida();
+	                	jogoIniciado = true;
+	            	}
+	            	
+	                break;
+	            case "5":
+	        		System.out.print("Você realmente quer finalizar o jogo? (1 - sim, 2 - não): ");
+	                opcao = entrada.next();
+	                
+	                if(jogoIniciado) {
+	                    if(opcao.equals("1")) jogoIniciado = false;
+	                } else {
+	                	opcao = "5";
+	                }
+                    
+	                break;
+	            default:
+	            	if(!opcao.equals("0"))
+	            		System.out.println("Opção inválida.");
+            }
+        } while (!opcao.equals("5"));
+		
+		entrada.close();
+	}
+	
 	public static void main(String[] args) {
 		new Partida();
+		menu();
 	}
+
 }
