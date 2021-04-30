5c75cb62eb74b7acc60003fc78986abbd8b9a762
Matheus Teles <matheustdo1@gmail.com>
2021-04-30 00:11:55 -0300

    Muda os métodos da view

--- src/controller/Fachada.java
+++ src/controller/Fachada.java
@@ -1,5 +1,17 @@
 package controller;
 
 public class Fachada {
-
+	private Paciencia paciencia = null;
+	
+	public boolean temPartidaAtiva() {
+		return this.paciencia != null;
+	}
+	
+	public void iniciarPartida() {
+		this.paciencia = new Paciencia();
+	}
+	
+	public void encerrarPartida() {
+		this.paciencia = null;
+	}
 }
