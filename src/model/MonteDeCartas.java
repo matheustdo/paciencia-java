842ee31f2810f56de8b95dce0814119dd33032cb
Rafael Azevedo <rrazvd@gmail.com>
2021-05-02 23:11:16 -0300

    Adiciona comentários a Classe Monte de Cartas

--- src/model/MonteDeCartas.java
+++ src/model/MonteDeCartas.java
@@ -2,19 +2,32 @@ package model;
 
 import java.util.Stack;
 
+/*
+* Esta classe representa um monte de cartas.
+*/
 public class MonteDeCartas {
 	
-	protected Stack<Carta> cartas;
+	protected Stack<Carta> cartas; // um monte é representado por uma pilha
 	
 	public MonteDeCartas() {
 		this.cartas = new Stack<Carta>();
 	}
 	
+	/**
+	* Esta função define como o monte de cartas deve receber uma carta.
+	* @param carta a ser recebida.
+	* @param monte de origem.
+	* @return se a carta foi recebida ou não
+	*/
 	public boolean receberCarta(Carta carta, MonteDeCartas origem) {
 		this.cartas.push(carta);
 		return true;
 	}
-
+	
+	/*
+	* Esta função permite retirar uma carta do topo.
+	* @return carta do topo que foi retirada.
+	*/
 	public Carta retirarCartaDoTopo() {
 		if (!cartas.isEmpty()) {
 			return this.cartas.pop();
@@ -22,6 +35,10 @@ public class MonteDeCartas {
 		return null;
 	}
 	
+	/*
+	* Esta função permite visualizar uma carta do topo.
+	* @return carta do topo para visualização.
+	*/
 	public Carta visualizarCartaDoTopo() {
 		if (!cartas.isEmpty()) {
 			return this.cartas.peek();
@@ -29,6 +46,10 @@ public class MonteDeCartas {
 		return null;
 	}
 	
+	/*
+	* Esta função permite virar uma carta do topo.
+	* @return carta do topo que foi virada.
+	*/
 	public Carta virarCartaDoTopo() {
 		Carta topo = visualizarCartaDoTopo();
 		
@@ -39,7 +60,10 @@ public class MonteDeCartas {
 		return topo;
 	}
 	
-	
+	/*
+	* Esta função retorna se o monte está vazio.
+	* @return se está vazio ou não.
+	*/
 	public boolean estaVazio() {
 		return cartas.isEmpty();
 	}
