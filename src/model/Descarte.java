6cbbf7e1af3ceed6bf7e5b1b7ca768cd3ec39473
Rafael Azevedo <rrazvd@gmail.com>
2021-05-02 22:52:09 -0300

    Adiciona comentários na classe Descarte

--- src/model/Descarte.java
+++ src/model/Descarte.java
@@ -1,12 +1,21 @@
 package model;
 import model.Estoque;
 
+/**
+* Esta classe representa um Monte de Cartas do tipo Descarte.
+*/
 public class Descarte extends MonteDeCartas{
 	
 	public Descarte () {
 		super();
 	}
 	
+	/*
+	* Esta função sobrescrita implementa como Estoque recebe uma carta.
+	* @param carta a ser recebida.
+	* @param monte de origem.
+	* @return se a carta foi recebida ou não.
+	*/
 	@Override
 	public boolean receberCarta(Carta carta, MonteDeCartas origem) {
 		if (origem instanceof Estoque) {
