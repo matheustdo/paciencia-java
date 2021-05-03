6cd1a6fbc25d77f2eb3bf2d0fd6b65295b4bce78
Rafael Azevedo <rrazvd@gmail.com>
2021-05-02 22:44:08 -0300

    Adiciona comentários no controlador Paciência.

--- src/controller/Paciencia.java
+++ src/controller/Paciencia.java
@@ -12,6 +12,9 @@ import model.Fileira;
 import model.Estoque;
 import model.Descarte;
 
+/**
+* Essa classe representa o controlador do jogo Paciência.
+*/
 public class Paciencia {
 	private static final int QUANTIDADE_FUNDACOES = 4;
 	private static final int QUANTIDADE_FILEIRAS = 7;
@@ -20,31 +23,49 @@ public class Paciencia {
 	private MonteDeCartas descarte = new Descarte();
 	private ArrayList<MonteDeCartas> montes = new ArrayList<MonteDeCartas>(2 + QUANTIDADE_FILEIRAS + QUANTIDADE_FUNDACOES);
 	
+	/**
+	* Este construtor permite estabelecer o estado inicial do jogo.
+	*/
 	public Paciencia() {
+		
+		/*Adiciona estoque e descarte a lista de montes*/
 		montes.add(estoque);
 		montes.add(descarte);
-
+		
+		/*Adiciona as fundações a lista de montes*/
 		for(int i = 0; i < QUANTIDADE_FUNDACOES; i++) {
 			Fundacao fundacao = new Fundacao();
 			montes.add(fundacao);
 		}
 		
+		/*Adiciona as fileiras a lista de montes*/
 		for(int i = 0; i < QUANTIDADE_FILEIRAS; i++) {
 			Fileira fileira = new Fileira();
-			for(int j = 0; j <= i; j++) {
+			
+			/*Preenche as fileiras de cartas e vira a carta do topo*/
+			for(int j = 0; j <= i; j++) { 
 				Carta carta = estoque.retirarCartaDoTopo();
 				fileira.preencher(carta);
 			}
-			fileira.virarCartaDoTopo();
+			fileira.virarCartaDoTopo(); 
 			montes.add(fileira);
 		}
 	}
 	
-	
+	/**
+	* Essa função permite definir a quantidade de cartas a serem viradas no estoque.
+	* @param quantidade de cartas.
+	*/
 	public void definirQtdVirarEstoque (int n) {
 		qtdCartasVirarEstoque = n;
 	}
 	
+	/**
+	* Essa função permite o jogador mover a carta de um monte para outro.
+	* @param identificador do monte de origem.
+	* @param identificador do monte de destino.
+	* @return se foi possível realizar o movimento ou não.
+	*/
 	public boolean moverCarta(int idOrigem, int idDestino) {
 		if(idOrigem  < 1 || idDestino < 1) return false;
 			
@@ -85,10 +106,18 @@ public class Paciencia {
 		} 
 	}
 	
+	/**
+	* Essa função retira carta do estoque para o descarte.
+	* @return se retirou ou não.
+	*/
 	public boolean exibirCarta() {
-		return moverCarta(1, 2);
+		return moverCarta(1, 2); // 1 é estoque, 2 é descarte
 	}
 	
+	/**
+	* Essa função verifica se o jogador venceu.
+	* @return se o jogador venceu ou não.
+	*/
 	public boolean verificarVitoria() {
 		int qtdFundacoesCompletas = 0;
 		for(MonteDeCartas monte : montes) {
@@ -105,6 +134,11 @@ public class Paciencia {
 		return false;
 	}
 	
+	/**
+	* Essa função verifica se uma fileira possui uma sequência de cartas.
+	* @param identificador da fileira.
+	* @return se tem sequencia na fileira ou não.
+	*/
 	public boolean temSequenciaNaFileira(int idMonte) {
 		if (idMonte < 1) return false;
 		MonteDeCartas mdc = montes.get(idMonte - 1);
@@ -138,6 +172,13 @@ public class Paciencia {
 		return false;
 	}
 	
+	/**
+	* Esta função permite o jogador mover uma sequência de uma fileira para outra.
+	* @param identificador do monte de origem. 
+	* @param identificador do monte de destino.
+	* @param quantidade de cartas que deseja mover.
+	* @return se a sequência foi movida ou não.
+	*/
 	public boolean moverSequencia(int idOrigem, int idDestino, int quantidadeCartas) {
 		if(idOrigem  < 1 || idDestino < 1) return false;
 		
@@ -190,7 +231,6 @@ public class Paciencia {
 		return false;
 	}
 	
-
 	@Override
 	public String toString() {
 		String opcoes = "";
